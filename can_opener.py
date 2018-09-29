f983f372cfbbf399a41dbfef6756854d9b023e0e
Praxis | Darian <hello@vulpinecat.com>
2018-09-29 14:42:19 +0200

    add filescan to scan for compatible files

--- can_opener.py
+++ can_opener.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello World")
+import os
+import asammdf
+
+
+# scans the import directory for compatible files
+def filescan():
+    compatible_files = 0
+    for file in os.listdir('import'):
+        if file[-4:] == ".mf4":
+            print("compatible file found: " + file)
+            compatible_files += 1
+
+    print("Total of", compatible_files, "compatible files found!")
 
 
 if __name__ == "__main__":
-    main()
+    filescan()
+    print("\nTo use this package, please import it into a life shell or into your script.\n"
+          "Direct execution of this package only yields a file scan!.")
